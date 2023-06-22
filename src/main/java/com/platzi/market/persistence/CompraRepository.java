3394db357605bb48a01033232e179143023846f7
Shirley Tatiana <stgarciac@ufpso.edu.co>
2023-06-22 15:10:29 -0500

    Version1

---
+++ src/main/java/com/platzi/market/persistence/CompraRepository.java
@@ -0,0 +1,37 @@
+package com.platzi.market.persistence;
+
+import com.platzi.market.domain.Purchase;
+import com.platzi.market.domain.repository.PurchaseRepository;
+import com.platzi.market.persistence.crud.compraCrudRepository;
+import com.platzi.market.persistence.entity.Compra;
+import com.platzi.market.persistence.mapper.PurchaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CompraRepository implements PurchaseRepository {
+    @Autowired
+    private compraCrudRepository compraCrudRepository;
+    @Autowired
+    private PurchaseMapper mapper;
+    @Override
+    public List<Purchase> getAll() {
+        return mapper.toPurchases((List<Compra>) compraCrudRepository.findAll());
+    }
+
+    @Override
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return compraCrudRepository.findByIdCliente(clientId)
+                .map(compras -> mapper.toPurchases(compras));
+    }
+
+    @Override
+    public Purchase save(Purchase purchase) {
+        Compra compra= mapper.ToCompra(purchase);
+        compra.getProductos().forEach(producto -> producto.setCompra(compra));
+        return mapper.toPurchase(compraCrudRepository.save(compra));
+    }
+}
---
+++ src/main/java/com/platzi/market/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mappings;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source="idCategoria", target="categoryId"),
+            @Mapping(source="descripcion", target="category"),
+            @Mapping(source="estado", target="active")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore= true)
+    Categoria toCategoria(Category category);
+}
